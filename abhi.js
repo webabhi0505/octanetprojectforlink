edd8e4fe219a2d3930cc8befcde58f9ff7ce4f9b
webabhi0505 <135849277+webabhi0505@users.noreply.github.com>
2023-06-23 00:08:22 +0530

    Add files via upload

---
+++ abhi.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box");
+const listConatiner = document.getElementById("list-conatiner");
+
+function butoon1(){
+    if(inputBox.value === ''){
+        alert("you must write something!")
+    }
+
+    else{
+        let li = document.createElement("li");
+        li.innerHTML= inputBox.value;
+listConatiner.appendChild(li);
+let span =document.createElement("span");
+
+span.innerHTML= "\u00d7";
+li.appendChild(span);
+
+
+}
+  inputBox.value= "";
+  saveData();
+}
+
+listConatiner.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if( e.target.tagName === "SPAN"   ){
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false);
+
+function saveData() {
+    localStorage.setItem("data",listConatiner.innerHTML);
+}
+function showTask(){
+    listConatiner.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
+
